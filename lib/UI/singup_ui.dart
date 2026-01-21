f95e4f752cbdd061c55ba18123565620e364edd1
OrionZodiac <apisitleo.soda@gmail.com>
2026-01-21 11:47:05 +0700

    ทำsingup เสร็จแล้ว

--- lib/UI/singup_ui.dart
+++ lib/UI/singup_ui.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SingupUi extends StatefulWidget {
   const SingupUi({super.key});
@@ -12,6 +15,214 @@ class _SingupUiState extends State<SingupUi> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[50],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Center(
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios_new,
+                      size: 35,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset(
+                    'assets/image/logo.png',
+                    width: 150.0,
+                    height: 160.0,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Get On Board !',
+                    style: TextStyle(
+                      fontSize: 30.00,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Create your profile to start your journey',
+                    style: TextStyle(
+                      fontSize: 15.00,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.00,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(Icons.person),
+                    hintText: 'Name',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 25.00,
+                      horizontal: 20.00,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.00,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(Icons.mail),
+                    hintText: 'E-mail',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 25.00,
+                      horizontal: 20.00,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.00,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(Icons.phone),
+                    hintText: 'Phone Number',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 25.00,
+                      horizontal: 20.00,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.00,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(FontAwesomeIcons.fingerprint),
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 25.00,
+                      horizontal: 20.00,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.00,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SING UP',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      50,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(10.0)),
+                    backgroundColor: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  height: 15.00,
+                ),
+                Text('OR'),
+                SizedBox(
+                  height: 15.00,
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/image/googlelogo.png',
+                        width: 20,
+                        height: 20,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text(
+                        'Sing Up with Google',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: OutlinedButton.styleFrom(
+                      fixedSize: Size(
+                        MediaQuery.of(context).size.width,
+                        50.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(10.0))),
+                ),
+                SizedBox(
+                  height: 15.00,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Already have an Account ?'),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
